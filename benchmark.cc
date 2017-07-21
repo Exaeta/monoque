82b2781df1c30e64bbb7ea1297df9b0bc4704337
Tino Didriksen <mail@tinodidriksen.com>
2017-07-21 11:56:27 +0200

    Better benchmarking

--- benchmark.cc
+++ benchmark.cc
@@ -2,6 +2,7 @@
 #include <random>
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <chrono>
 #include <deque>
@@ -14,42 +15,18 @@ int main()
   using namespace std::chrono;
   using namespace rpnx;
 
-  std::ofstream o1("monoque_benchmark.txt");
-  //  std::ofstream o1r("monoque::operator[]const.txt");
-  std::ofstream o2("vector_benchmark.txt");
-  // std::ofstream o2r("vector::operator[]const.txt");
-  std::ofstream o3("deque_benchmark.txt");
-
   mt19937_64 r;
- 
+
   size_t round_count = 300000000;
   size_t round_secondary = round_count*4;
+  size_t runs = 3;
 
-  /*
-  {
-    monoque<int> m;
-    for (int i = 0; i < round_count; i++)
-    {
-      m.push_back(i);
-    }
-  }
-
-  {
-    vector<int> m;
-    for (int i = 0; i < round_count; i++)
-    {
-      m.push_back(i);
-    }
-  }
-  */
-  
   vector<size_t> rds;
-
-  for (int i = 0; i < round_count; i++)
+  for (size_t i = 0; i < round_count; i++)
     {
       rds.push_back(r() % round_count);
     }
-  
+
   system_clock::time_point start_time;
   system_clock::time_point end_time;
 
@@ -57,119 +34,98 @@ int main()
   system_clock::time_point end_time_tt;
 
   cout  << "For linear structures of size " << round_count << " and random access " << round_secondary << " times, discovered that: " << endl;
- 
+
+  double fast_push = std::numeric_limits<double>::max();
+  double fast_access = std::numeric_limits<double>::max();
+  cout << fixed;
+
+
+  fast_push = std::numeric_limits<double>::max();
+  fast_access = std::numeric_limits<double>::max();
+  for (size_t r=0 ; r<runs ; ++r)
   {
-    vector<int> m;
+    vector<size_t> m;
     start_time = system_clock::now();
 
-    volatile int tester;
-  
     for (size_t i = 0; i < round_count; i++)
     {
-      tester = i;      
-      m.push_back(int(tester));    
-    }
-    end_time = system_clock::now();
-    cout << "vector<int> average push_back time: " << (double)(duration_cast<nanoseconds>(end_time-start_time).count())/round_count << " nanoseconds" << endl;
-    this_thread::yield();
-    
-    start_time = system_clock::now();
-    for (size_t i = 0; i < round_secondary; i++)
-    {     
-      tester = m[rds[i % round_count]];
-    }
-    end_time = system_clock::now();
-    cout << "vector<int> average random access time: " << (double)(duration_cast<nanoseconds>(end_time-start_time).count())/round_secondary << " nanoseconds" << endl;
-    
-  }
-
-  {
-    monoque<int> m;
-    start_time = system_clock::now();
-
-    volatile int tester;
-  
-    for (size_t i = 0; i < round_count; i++)
-    {
-      tester = i;      
-      m.push_back(int(tester));    
-    }
-    end_time = system_clock::now();
-    cout << "monoque<int> average push_back time: " << (double)(duration_cast<nanoseconds>(end_time-start_time).count())/round_count << " nanoseconds" << endl;
-    this_thread::yield();
-    
-    start_time = system_clock::now();
-    for (size_t i = 0; i < round_secondary; i++)
-    {     
-      tester = m[rds[i % round_count]];
-    }
-    end_time = system_clock::now();
-    cout << "monoque<int> average random access time: " << (double)(duration_cast<nanoseconds>(end_time-start_time).count())/round_secondary << " nanoseconds" << endl;
-    
-  }
-
-
-
- /*
-  {
-
-
-    vector<int> m;
-    for (int  i = 0; i < round_count; i++)
-    {
-      start_time = system_clock::now();
-      std::atomic_signal_fence(memory_order_seq_cst);
       m.push_back(i);
-      std::atomic_signal_fence(memory_order_seq_cst);
-      end_time = system_clock::now();
-      o2 << duration_cast<nanoseconds>(end_time-start_time).count() << endl;
-      this_thread::yield();
-    }
-
-
-    start_time = system_clock::now();
-    volatile int tester;
-    for (int i = 0; i < round_count; i++)
-    {
-      std::atomic_signal_fence(memory_order_seq_cst);
-      tester = m[rds[i]];      
-      std::atomic_signal_fence(memory_order_seq_cst);
     }
     end_time = system_clock::now();
-    //duration_cast<nanoseconds>(end_time-start_time).count())/round_count << endl;
+    fast_push = std::min(fast_push, (double)(duration_cast<nanoseconds>(end_time-start_time).count()));
+    cout << m.size() << ' ' << fast_push << endl;
+
+    start_time = system_clock::now();
+    size_t t = 0;
+    for (size_t i = 0; i < round_secondary; i++)
+    {
+      t += m[rds[i % round_count]];
+    }
+    end_time = system_clock::now();
+    fast_access = std::min(fast_access, (double)(duration_cast<nanoseconds>(end_time-start_time).count()));
+    cout << t << ' ' << fast_access << endl;
 
   }
- */
-  
+  cout << "vector<size_t> average push_back time: " << fast_push/round_count << " nanoseconds" << endl;
+  cout << "vector<size_t> average random access time: " << fast_access/round_secondary << " nanoseconds" << endl;
 
 
+  fast_push = std::numeric_limits<double>::max();
+  fast_access = std::numeric_limits<double>::max();
+  for (size_t r=0 ; r<runs ; ++r)
   {
-   
-    deque<int> m;
-    volatile int tester;
+    monoque<size_t> m;
+    start_time = system_clock::now();
+
+    for (size_t i = 0; i < round_count; i++)
+    {
+      m.push_back(i);
+    }
+    end_time = system_clock::now();
+    fast_push = std::min(fast_push, (double)(duration_cast<nanoseconds>(end_time-start_time).count()));
+    cout << m.size() << ' ' << fast_push << endl;
+
+    start_time = system_clock::now();
+    size_t t = 0;
+    for (size_t i = 0; i < round_secondary; i++)
+    {
+      t += m[rds[i % round_count]];
+    }
+    end_time = system_clock::now();
+    fast_access = std::min(fast_access, (double)(duration_cast<nanoseconds>(end_time-start_time).count()));
+    cout << t << ' ' << fast_access << endl;
+
+  }
+  cout << "monoque<size_t> average push_back time: " << fast_push/round_count << " nanoseconds" << endl;
+  cout << "monoque<size_t> average random access time: " << fast_access/round_secondary << " nanoseconds" << endl;
+
+
+  fast_push = std::numeric_limits<double>::max();
+  fast_access = std::numeric_limits<double>::max();
+  for (size_t r=0 ; r<runs ; ++r)
+  {
+    deque<size_t> m;
 
     start_time = system_clock::now();
     for (size_t  i = 0; i < round_count; i++)
     {
-      tester = i;
-      m.push_back(int(tester));
+      m.push_back(i);
     }
     end_time = system_clock::now();
-    cout << "deque<int> average push_back time: " << double(duration_cast<nanoseconds>(end_time-start_time).count())/round_count <<  " nanoseconds" << endl;
-    this_thread::yield();
+    fast_push = std::min(fast_push, (double)(duration_cast<nanoseconds>(end_time-start_time).count()));
+    cout << m.size() << ' ' << fast_push << endl;
 
     start_time = system_clock::now();
-    
+    size_t t = 0;
     for (size_t i = 0; i < round_secondary; i++)
     {
-      tester = m[rds[i % round_count]];
+      t += m[rds[i % round_count]];
     }
-
     end_time = system_clock::now();
-    cout << "deque<int> average random access time: " << (double)(duration_cast<nanoseconds>(end_time-start_time).count())/round_secondary << " nanoseconds" << endl;
+    fast_access = std::min(fast_access, (double)(duration_cast<nanoseconds>(end_time-start_time).count()));
+    cout << t << ' ' << fast_access << endl;
 
   }
-
-  return 0;
-
+  cout << "deque<size_t> average push_back time: " << fast_push/round_count <<  " nanoseconds" << endl;
+  cout << "deque<size_t> average random access time: " << fast_access/round_secondary << " nanoseconds" << endl;
 }
