5568d675bae4a785aa3b8a0cf4bd3754e5eda65d
Ryan P. Nicholl <archzombielord@gmail.com>
2017-07-21 02:29:53 -0400

    Optimize data structure and start to implement some STL support
    Optimize data structure and start to implement some STL support

--- monoque.hh
+++ monoque.hh
@@ -27,7 +27,7 @@ SOFTWARE.
 
 #include <array>
 #include <tuple>
-
+#include <atomic>
 /*
   Like vector, but non-contiguous and has worst-case O(1) push_back and worst-case O(1) indexing.
   This is just a simple proof of concept at the present time. It is not a complete STL compatible
@@ -36,44 +36,193 @@ SOFTWARE.
   TODO: Allocator Support, Iterator Support, STL Container Support.
 
  */
+
+#ifndef rpnx_expect
+#if false
+#define rpnx_expect(x, y) x
+#else
+#define rpnx_expect(x, y) __builtin_expect(x, y)
+#endif
+#define rpnx_likely(x) rpnx_expect(x, 1)
+#define rpnx_unlikely(x) rpnx_expect(x, 0)
+#endif
+
+
+
 namespace rpnx{
-template <typename T>
+template <typename T, typename Allocator = std::allocator<T> >
 class monoque
 {
-  std::array<T*, sizeof(T*)*8> data_pv;
   size_t size_pv;
+  std::array<T*, sizeof(T*)*8>   data_pv  ;
+
   //size_t cap;
 private:
  
-  static inline size_t index1_pv(size_t n)
+  static inline size_t index1_pv(size_t n) __attribute__((always_inline))  
   {
-#ifdef __x86_64__
-    size_t i = 0;    
-    asm("bsrq %1,%0" : "=r"(i) : "r"(n));
-    return n == 0 ? 0 : i;
+#ifdef __x86_64__ 
+    if (rpnx_unlikely(n==0)) return 0;
+    else
+      {
+        size_t i;
+        asm("bsrq %1,%0\n" : "=r"(i) : "r"(n));
+        return i;
+      }
 #else
     int i = 0; while ((1 << i) <= n) i++;
-#endif
     return i == 0 ? 0 : i - 1;
+#endif   
   }
 
-  static inline size_t sizeat_pv(size_t at)
+  static inline size_t sizeat_pv(size_t at)  __attribute__((always_inline))
   {
-    return at <= 1 ? 2 : (1 << index1_pv(at));
+    if (rpnx_unlikely(at <= 1)) return 2;
+    else return (size_t(1) << index1_pv(at));
   }
  
-  static inline size_t index2_pv(size_t at)
+
+  static inline size_t index2_pv(size_t n) __attribute__((always_inline))
   {
-    return at & (sizeat_pv(at)-1);
+#if defined(__x86_64__) //&& !defined(__clang__)
+    // No idea why, but for some reason this is slow as fuck in clang -O2.
+    if (rpnx_unlikely(n<=1)) return n&1;
+    else
+      {
+        size_t i;
+        asm("bsrq %1,%0\n" : "=r"(i) : "r"(n));
+        return n ^ (size_t(1) << i);
+      }
+#else
+  return n & (sizeat_pv(n)-1);
+#endif
   }
   
-  static inline std::tuple<size_t, size_t> index_pv(size_t at)
+  static inline std::tuple<size_t, size_t> index_pv(size_t at)  __attribute__((always_inline))
   {
     return {index1_pv(at), index2_pv(at)};
   }
 
   
 public:
+  using value_type = T;
+  using allocator_type = Allocator;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+  using referece = value_type&;
+  using const_reference = value_type const &;
+  using pointer = typename std::allocator_traits<Allocator>::pointer;
+  using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
+
+  class iterator
+  {
+    monoque *m;
+    size_type i;
+  public:
+    iterator()
+      : m (nullptr), i(0)
+    {
+    }
+
+    using difference_type = std::ptrdiff_t;
+    using value_type = T;
+    using pointer =typename std::allocator_traits<Allocator>::pointer;
+
+    iterator(const iterator&)=default;
+    iterator(iterator &&)=default;
+
+    iterator & operator=(iterator const&)=default;
+    iterator & operator=(iterator &&)=default;
+
+    T  & operator*() const
+    {
+      return (*m)[i];
+    }
+
+    iterator & operator++()
+    {
+      i++;
+      return *this;
+    }
+
+    iterator operator++(int)
+    {
+      iterator copy = *this;
+      i++;
+      return copy;
+    }
+
+    iterator & operator +=(difference_type n)
+    {
+      i+=n;
+      return *this;
+    }
+
+    iterator & operator -=(difference_type n)
+    {
+      i-=n;
+      return *this;
+    }
+
+    value_type & operator[](difference_type n) const
+    {
+      return *(*this+i);
+    }
+
+    difference_type operator - (iterator const & other) const
+    {
+      return i - other.i;
+    }
+
+    iterator operator +(difference_type n) const
+    {
+      iterator copy = *this;
+      copy.i += n;
+      return copy;
+    }
+
+
+    iterator operator -(difference_type n) const
+    {
+      iterator copy = *this;
+      copy.i -= n;
+      return copy;
+    }
+
+    bool operator ==(iterator const &o) const
+    {
+      return m == o.m && i == o.i;
+    }
+
+
+    bool operator !=(iterator const &o) const
+    {
+      return m != o.m && i != o.i;
+    }
+
+    bool operator <(iterator const &o) const
+    {
+      return i < o.i;
+    }
+    
+    bool operator <=(iterator const &o) const
+    {
+      return i <= o.i;
+    }
+
+    bool operator >(iterator const &o) const
+    {
+      return i > o.i;
+    }
+    
+    bool operator >=(iterator const &o) const
+    {
+      return i >= o.i;
+    }
+  };
+
+  using reverse_iterator = std::reverse_iterator<iterator>;
+
   monoque()
     : size_pv(0)
   {
@@ -87,27 +236,39 @@ public:
     for (auto & x: data_pv) if (x != nullptr) delete[] (char*) x;
   }
 
-  T & operator[](size_t at)
+  inline T & operator[](size_t at)  //__attribute__((always_inline)) 
   {
+
     using namespace std;
+#if  defined(__clang__)
+     std::atomic_signal_fence(memory_order_seq_cst);
+#endif
 
     size_t index1;
     size_t index2;
-    
-    tie(index1, index2) = index_pv(at);
+
+    index1 = index1_pv(at);
+    index2 = index2_pv(at);
+    //    index2 = index2_pv(at);
+
 
     return data_pv[index1][index2];
   }
 
 
-  T const & operator[](size_t at) const
+  inline T const & operator[](size_t at) const //  __attribute__((always_inline)) 
   {
     using namespace std;
+
+#if defined(__clang__)
+    std::atomic_signal_fence(memory_order_seq_cst);
+#endif
 
     size_t index1;
     size_t index2;
     
     tie(index1, index2) = index_pv(at);
+    //    tie(index2) = index2_pv(at);
 
     return data_pv[index1][index2];
   }
@@ -118,7 +279,7 @@ public:
     return size_pv;
   }
 
-  void push_back (T t)
+  inline void push_back (T t)
   {
     if (index2_pv(size_pv) == 0)
       {
@@ -133,6 +294,21 @@ public:
   {
     this->operator[](size_pv-1).T::~T();
     size_pv--;
+  }
+
+  void shink_to_fit()
+  {
+    size_t mindex = 0;
+    if (size_pv != 0) mindex = index1_pv(size_pv-1) + 1;
+    for (size_t i = mindex; i < sizeof(T*)*8; i++)
+      {
+        if (data_pv[i] != nullptr) 
+          {
+            delete[] (char*) data_pv[i];
+            data_pv[i] = nullptr;
+          }
+        else break;
+      }    
   }
 
   
